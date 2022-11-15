af01efc00fccbe0fc0fa0805136ed28d14cea775
Valdano Simão <valdanosimao@gmail.com>
2022-11-15 14:01:40 -0300

    implementando a estilização do main activity e evento de click

--- app/src/main/java/br/com/motivation/MainActivity.kt
+++ app/src/main/java/br/com/motivation/MainActivity.kt
@@ -2,10 +2,28 @@ package br.com.motivation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import br.com.motivation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Esconde a barra de navegação
+        supportActionBar?.hide()
+
+        //Eventos
+        binding.btnNewPhrase.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+           if(view.id == R.id.btn_new_phrase){
+               var s = ""
+           }
     }
 }
